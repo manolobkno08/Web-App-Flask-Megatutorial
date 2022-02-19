1b3894ddf4767f0746acdf91fe7fcee1700bc07c
manolobkno08 <manologm10@hotmail.com>
2022-02-19 17:32:49 -0500

    ADD: New comments

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,9 @@ from flask import Flask
 from config import Config
 
 app = Flask(__name__)
-app.config.from_object(Config)
-
+app.config.from_object(Config) #app.config['SECRET_KEY'] = 'you-will-never-guess'
+#print(dir(Config))
+# Import routes above to create app object
 from app import routes
 
 # if __name__ == '__main__':
--- app/routes.py
+++ app/routes.py
@@ -29,9 +29,7 @@ def login():
     if form.validate_on_submit():
         flash(
             f'Login requested for user: {form.username.data}, remember_me: {form.remember_me.data}')
-        print("YEEEEHHHHHHHHHH")
-        print(len(form.username.data))
+        print("Data capture successful")
         return redirect(url_for('index'))
 
-    print("outside")
     return render_template('login.html', title='Sign In', form=form)
