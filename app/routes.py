746fbcc1bed1963cd043fc9e4d2802372e8ed146
manolobkno08 <manologm10@hotmail.com>
2022-02-19 12:22:04 -0500

    ADD: Forms, routes and templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -19,3 +20,17 @@ def index():
     ]
 
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """Redirect and capture data from form submitted"""
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(
+            f'Login requested for user {form.username.data}, remember_me{form.remember_me.data}')
+        print("YEEEEHHHHHHHHHH")
+        return redirect('/index')
+
+    print("outside")
+    return render_template('login.html', title='Sign In', form=form)
