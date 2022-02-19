ceb23da75cc657a8dde53657ddbfb66750cde1c6
manolobkno08 <manologm10@hotmail.com>
2022-02-19 13:18:56 -0500

    ADD: Handle requirements and url_for method

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from app import app
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 
 
@@ -28,9 +28,10 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash(
-            f'Login requested for user {form.username.data}, remember_me{form.remember_me.data}')
+            f'Login requested for user: {form.username.data}, remember_me: {form.remember_me.data}')
         print("YEEEEHHHHHHHHHH")
-        return redirect('/index')
+        print(len(form.username.data))
+        return redirect(url_for('index'))
 
     print("outside")
     return render_template('login.html', title='Sign In', form=form)
