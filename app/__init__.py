fb20304e3b8c409dfa36f1635782fb8e9e1f03b9
manolobkno08 <manologm10@hotmail.com>
2022-02-20 21:09:43 -0500

    ADD: Consolidate database structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config) #app.config['SECRET_KEY'] = 'you-will-never-guess'
 #print(dir(Config))
-# Import routes above to create app object
-from app import routes
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# Import routes above to create app object
+from app import routes, models
+
